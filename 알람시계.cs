9f852acdbe94d2d7260c55493f4ea94f7c60798b
S-T Hwang <hst007@naver.com>
2021-10-16 21:13:19 +0900

    알람시계

---
+++ 알람시계.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BaekJoon1016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string[] a = Console.ReadLine().Split(' ');
+            int H = int.Parse(a[0]);
+            int M = int.Parse(a[1]);
+
+            if (H == 0)
+            {
+                if (M < 45)
+                {
+                    H = 23;
+                    M = M + 60 - 45;
+                    Console.WriteLine("{0} {1}", H, M);
+                }
+                else
+                {
+                    M -= 45;
+                    Console.WriteLine("{0} {1}", H, M);
+                }
+            }
+            else
+            {
+                if (M < 45)
+                {
+                    H--;
+                    M = M + 60 - 45;
+                    Console.WriteLine("{0} {1}", H, M);
+                }
+                else
+                {
+                    M -= 45;
+                    Console.WriteLine("{0} {1}", H, M);
+                }
+            }
+        }
+    }
+}
