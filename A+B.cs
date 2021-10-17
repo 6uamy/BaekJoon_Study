78cdd8239e61b8e8de0434303cb700a928cba7db
S-T Hwang <hst007@naver.com>
2021-10-17 22:35:52 +0900

    A+B

---
+++ A+B.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BaekJoon1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int i;
+            for (i = 0; i < T; i++)
+            {
+                string[] a = Console.ReadLine().Split(' ');
+                int A = int.Parse(a[0]);
+                int B = int.Parse(a[1]);
+
+                Console.WriteLine("{0}", A + B);
+            }
+
+        }
+    }
+}
