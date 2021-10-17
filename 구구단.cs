73580f7ecb90fdb81482c0a5e83292a3b300b8a4
S-T Hwang <hst007@naver.com>
2021-10-17 22:24:10 +0900

    구구단

---
+++ 구구단.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BaekJoon1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M,R;
+            for(M = 1; M <= 9; M++)
+            {
+                R = N * M;
+                Console.WriteLine("{0} * {1} = {2}", N, M, R);
+            }
+        }
+    }
+}
