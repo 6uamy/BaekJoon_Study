6b030ba4ce3e1358a5f58f3316888b8494ee07c8
S-T Hwang <hst007@naver.com>
2021-10-17 22:46:16 +0900

    합

---
+++ 합.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BaekJoon1017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            int m = 0;
+            for (i = 1; i <= n; i++)
+            {
+                m += i;
+            }
+            Console.WriteLine("{0}", m);
+        }
+    }
+}
