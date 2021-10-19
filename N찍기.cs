5c3c5cc563bcb35856989bc8002c37c403bf24d8
S-T Hwang <hst007@naver.com>
2021-10-19 18:23:21 +0900

    N 찍기

---
+++ N찍기.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace BaekJoon1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            StringBuilder allNumbers = new StringBuilder();
+
+            for (int i = 1; i <= N; i++)
+            {
+                int number = N - (N - i);
+                allNumbers.Append(number + "\n");
+            }
+            Console.Write(allNumbers.ToString());
+        }
+    }
+}
