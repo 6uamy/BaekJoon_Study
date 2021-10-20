9ce67e1cfb45cb8c7b55af22253a9901c397e00c
S-T Hwang <hst007@naver.com>
2021-10-20 14:16:23 +0900

    기찍 N

---
+++ 기찍N.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace BaekJoon1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            StringBuilder allNumbers = new StringBuilder();
+
+            for (int i = N; i > 0; i--)
+            {
+                allNumbers.Append(i + "\n");
+            }
+            Console.Write(allNumbers.ToString());
+        }
+    }
+}
